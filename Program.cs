cc79ae563dc855c6617736288f283db773a99fdf
Gamer_Z <grasmanek94@gmail.com>
2015-04-29 11:37:25 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Resources;
+
+namespace CreateNewProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool good = false;
+
+			string currentPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+			string projectName = "";			
+			string projectPath = "";
+
+			while (!good)
+			{
+				Console.WriteLine("New Project Name:");
+				projectName = Console.ReadLine().Replace(" ", "").Replace("\t", "");			
+				try
+				{
+					projectPath = currentPath + "\\" + projectName + "\\";
+					Directory.CreateDirectory(projectPath);
+					good = true;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Invalid Project Name: " + ex.Message);
+				}
+			}
+
+
+			try
+			{
+				File.WriteAllText(projectPath + "CompileLinkAndUpload.bat", CreateNewProject.Properties.Resources.CompileLinkAndUpload);
+				File.WriteAllText(projectPath + "makefile", CreateNewProject.Properties.Resources.makefile);
+				File.WriteAllText(projectPath + projectName + ".c", CreateNewProject.Properties.Resources.Source.Replace("<PROJECTNAME>", projectName));
+				File.WriteAllText(projectPath + projectName + ".h", CreateNewProject.Properties.Resources.Header.Replace("<PROJECTNAME>", projectName));
+				File.WriteAllText(projectPath + projectName + ".sln", CreateNewProject.Properties.Resources.Solution.Replace("<PROJECTNAME>", projectName));
+				File.WriteAllText(projectPath + projectName + ".vcxproj", CreateNewProject.Properties.Resources.Project.Replace("<PROJECTNAME>", projectName));
+				File.WriteAllText(projectPath + projectName + ".vcxproj.filters", CreateNewProject.Properties.Resources.Filters.Replace("<PROJECTNAME>", projectName));
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Project Creation Failed with message: " + ex.Message);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+This program creates projects for RP6 development using Visual Studio 2013.
+
+Don't forget to install the RP6 Development Environment for Visual Studio from:
+
+https://drive.google.com/file/d/0BzUKBMbvP0EFZ05seFE5QlppOWs/view?usp=sharing
